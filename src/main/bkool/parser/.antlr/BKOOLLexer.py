3a3ac76950f8a896d1d98ba2d3aa30ff7cdaecd4
student1060632fpt <haveaniceday6174@gmail.com>
2023-06-25 19:07:05 +0700

    xong b3

--- src/main/bkool/parser/.antlr/BKOOLLexer.py
+++ src/main/bkool/parser/.antlr/BKOOLLexer.py
@@ -1,4 +1,4 @@
-# Generated from f:\\BACH_KINH_XAY\\223\\ppl\\draft\\initial\\src\\main\\bkool\\parser\\BKOOL.g4 by ANTLR 4.9.2
+# Generated from f:\\BACH_KINH_XAY\\223\\ppl\\draft\\initial\\src\\main\\bkool\\parser\\BKOOL_b3.g4 by ANTLR 4.9.2
 from antlr4 import *
 from io import StringIO
 import sys
@@ -15,27 +15,27 @@ from lexererr import *
 def serializedATN():
     with StringIO() as buf:
         buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\7")
-        buf.write(";\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("=\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
         buf.write("\4\b\t\b\4\t\t\t\3\2\3\2\3\2\3\2\3\2\6\2\31\n\2\r\2\16")
-        buf.write("\2\32\7\2\35\n\2\f\2\16\2 \13\2\5\2\"\n\2\3\3\3\3\3\4")
-        buf.write("\7\4\'\n\4\f\4\16\4*\13\4\3\5\3\5\3\6\6\6/\n\6\r\6\16")
-        buf.write("\6\60\3\6\3\6\3\7\3\7\3\7\3\b\3\b\3\t\3\t\2\2\n\3\3\5")
-        buf.write("\2\7\2\t\2\13\4\r\5\17\6\21\7\3\2\5\3\2\62;\3\2\63;\5")
-        buf.write("\2\13\f\17\17\"\"\2<\2\3\3\2\2\2\2\13\3\2\2\2\2\r\3\2")
-        buf.write("\2\2\2\17\3\2\2\2\2\21\3\2\2\2\3!\3\2\2\2\5#\3\2\2\2\7")
-        buf.write("(\3\2\2\2\t+\3\2\2\2\13.\3\2\2\2\r\64\3\2\2\2\17\67\3")
-        buf.write("\2\2\2\219\3\2\2\2\23\"\5\5\3\2\24\25\5\t\5\2\25\36\5")
-        buf.write("\7\4\2\26\30\7a\2\2\27\31\t\2\2\2\30\27\3\2\2\2\31\32")
-        buf.write("\3\2\2\2\32\30\3\2\2\2\32\33\3\2\2\2\33\35\3\2\2\2\34")
-        buf.write("\26\3\2\2\2\35 \3\2\2\2\36\34\3\2\2\2\36\37\3\2\2\2\37")
-        buf.write("\"\3\2\2\2 \36\3\2\2\2!\23\3\2\2\2!\24\3\2\2\2\"\4\3\2")
-        buf.write("\2\2#$\7\62\2\2$\6\3\2\2\2%\'\t\2\2\2&%\3\2\2\2\'*\3\2")
-        buf.write("\2\2(&\3\2\2\2()\3\2\2\2)\b\3\2\2\2*(\3\2\2\2+,\t\3\2")
-        buf.write("\2,\n\3\2\2\2-/\t\4\2\2.-\3\2\2\2/\60\3\2\2\2\60.\3\2")
-        buf.write("\2\2\60\61\3\2\2\2\61\62\3\2\2\2\62\63\b\6\2\2\63\f\3")
-        buf.write("\2\2\2\64\65\13\2\2\2\65\66\b\7\3\2\66\16\3\2\2\2\678")
-        buf.write("\13\2\2\28\20\3\2\2\29:\13\2\2\2:\22\3\2\2\2\b\2\32\36")
-        buf.write("!(\60\4\b\2\2\3\7\2")
+        buf.write("\2\32\7\2\35\n\2\f\2\16\2 \13\2\3\2\3\2\5\2$\n\2\3\3\3")
+        buf.write("\3\3\4\7\4)\n\4\f\4\16\4,\13\4\3\5\3\5\3\6\6\6\61\n\6")
+        buf.write("\r\6\16\6\62\3\6\3\6\3\7\3\7\3\7\3\b\3\b\3\t\3\t\2\2\n")
+        buf.write("\3\3\5\2\7\2\t\2\13\4\r\5\17\6\21\7\3\2\5\3\2\62;\3\2")
+        buf.write("\63;\5\2\13\f\17\17\"\"\2>\2\3\3\2\2\2\2\13\3\2\2\2\2")
+        buf.write("\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\3#\3\2\2\2\5%\3\2")
+        buf.write("\2\2\7*\3\2\2\2\t-\3\2\2\2\13\60\3\2\2\2\r\66\3\2\2\2")
+        buf.write("\179\3\2\2\2\21;\3\2\2\2\23$\5\5\3\2\24\25\5\t\5\2\25")
+        buf.write("\36\5\7\4\2\26\30\7a\2\2\27\31\t\2\2\2\30\27\3\2\2\2\31")
+        buf.write("\32\3\2\2\2\32\30\3\2\2\2\32\33\3\2\2\2\33\35\3\2\2\2")
+        buf.write("\34\26\3\2\2\2\35 \3\2\2\2\36\34\3\2\2\2\36\37\3\2\2\2")
+        buf.write("\37!\3\2\2\2 \36\3\2\2\2!\"\b\2\2\2\"$\3\2\2\2#\23\3\2")
+        buf.write("\2\2#\24\3\2\2\2$\4\3\2\2\2%&\7\62\2\2&\6\3\2\2\2\')\t")
+        buf.write("\2\2\2(\'\3\2\2\2),\3\2\2\2*(\3\2\2\2*+\3\2\2\2+\b\3\2")
+        buf.write("\2\2,*\3\2\2\2-.\t\3\2\2.\n\3\2\2\2/\61\t\4\2\2\60/\3")
+        buf.write("\2\2\2\61\62\3\2\2\2\62\60\3\2\2\2\62\63\3\2\2\2\63\64")
+        buf.write("\3\2\2\2\64\65\b\6\3\2\65\f\3\2\2\2\66\67\13\2\2\2\67")
+        buf.write("8\b\7\4\28\16\3\2\2\29:\13\2\2\2:\20\3\2\2\2;<\13\2\2")
+        buf.write("\2<\22\3\2\2\2\b\2\32\36#*\62\5\3\2\2\b\2\2\3\7\3")
         return buf.getvalue()
 
 
@@ -64,7 +64,7 @@ class BKOOLLexer(Lexer):
     ruleNames = [ "INTLIT", "ONLY_ZERO", "NUMBER", "START_NO_ZERO", "WS", 
                   "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
 
-    grammarFileName = "BKOOL.g4"
+    grammarFileName = "BKOOL_b3.g4"
 
     def __init__(self, input=None, output:TextIO = sys.stdout):
         super().__init__(input, output)
@@ -77,6 +77,7 @@ class BKOOLLexer(Lexer):
     def action(self, localctx:RuleContext, ruleIndex:int, actionIndex:int):
         if self._actions is None:
             actions = dict()
+            actions[0] = self.INTLIT_action 
             actions[5] = self.ERROR_CHAR_action 
             self._actions = actions
         action = self._actions.get(ruleIndex, None)
@@ -86,8 +87,13 @@ class BKOOLLexer(Lexer):
             raise Exception("No registered action for:" + str(ruleIndex))
 
 
-    def ERROR_CHAR_action(self, localctx:RuleContext , actionIndex:int):
+    def INTLIT_action(self, localctx:RuleContext , actionIndex:int):
         if actionIndex == 0:
+            self.text = self.text.replace("_","")
+     
+
+    def ERROR_CHAR_action(self, localctx:RuleContext , actionIndex:int):
+        if actionIndex == 1:
             raise ErrorToken(self.text)
      
 
--- src/main/bkool/parser/BKOOLLexer.py
+++ src/main/bkool/parser/BKOOLLexer.py
@@ -15,28 +15,27 @@ from lexererr import *
 def serializedATN():
     with StringIO() as buf:
         buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\7")
-        buf.write("?\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
-        buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\3\2\3\2\3\2\3\2\3\2\3\2\3\3\3")
-        buf.write("\3\5\3\36\n\3\3\3\5\3!\n\3\3\3\5\3$\n\3\3\3\5\3\'\n\3")
-        buf.write("\3\4\6\4*\n\4\r\4\16\4+\3\5\3\5\3\6\3\6\3\7\6\7\63\n\7")
-        buf.write("\r\7\16\7\64\3\7\3\7\3\b\3\b\3\b\3\t\3\t\3\n\3\n\2\2\13")
-        buf.write("\3\3\5\2\7\2\t\2\13\2\r\4\17\5\21\6\23\7\3\2\6\3\2c|\6")
-        buf.write("\2\60\60\62;aac|\5\2\62;aac|\5\2\13\f\17\17\"\"\2@\2\3")
-        buf.write("\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3")
-        buf.write("\2\2\2\3\25\3\2\2\2\5\33\3\2\2\2\7)\3\2\2\2\t-\3\2\2\2")
-        buf.write("\13/\3\2\2\2\r\62\3\2\2\2\178\3\2\2\2\21;\3\2\2\2\23=")
-        buf.write("\3\2\2\2\25\26\5\7\4\2\26\27\7\60\2\2\27\30\5\7\4\2\30")
-        buf.write("\31\5\5\3\2\31\32\5\13\6\2\32\4\3\2\2\2\33\35\5\t\5\2")
-        buf.write("\34\36\5\t\5\2\35\34\3\2\2\2\35\36\3\2\2\2\36 \3\2\2\2")
-        buf.write("\37!\5\t\5\2 \37\3\2\2\2 !\3\2\2\2!#\3\2\2\2\"$\5\t\5")
-        buf.write("\2#\"\3\2\2\2#$\3\2\2\2$&\3\2\2\2%\'\5\t\5\2&%\3\2\2\2")
-        buf.write("&\'\3\2\2\2\'\6\3\2\2\2(*\t\2\2\2)(\3\2\2\2*+\3\2\2\2")
-        buf.write("+)\3\2\2\2+,\3\2\2\2,\b\3\2\2\2-.\t\3\2\2.\n\3\2\2\2/")
-        buf.write("\60\t\4\2\2\60\f\3\2\2\2\61\63\t\5\2\2\62\61\3\2\2\2\63")
-        buf.write("\64\3\2\2\2\64\62\3\2\2\2\64\65\3\2\2\2\65\66\3\2\2\2")
-        buf.write("\66\67\b\7\2\2\67\16\3\2\2\289\13\2\2\29:\b\b\3\2:\20")
-        buf.write("\3\2\2\2;<\13\2\2\2<\22\3\2\2\2=>\13\2\2\2>\24\3\2\2\2")
-        buf.write("\t\2\35 #&+\64\4\b\2\2\3\b\2")
+        buf.write("=\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("\4\b\t\b\4\t\t\t\3\2\3\2\3\2\3\2\3\2\6\2\31\n\2\r\2\16")
+        buf.write("\2\32\7\2\35\n\2\f\2\16\2 \13\2\3\2\3\2\5\2$\n\2\3\3\3")
+        buf.write("\3\3\4\7\4)\n\4\f\4\16\4,\13\4\3\5\3\5\3\6\6\6\61\n\6")
+        buf.write("\r\6\16\6\62\3\6\3\6\3\7\3\7\3\7\3\b\3\b\3\t\3\t\2\2\n")
+        buf.write("\3\3\5\2\7\2\t\2\13\4\r\5\17\6\21\7\3\2\5\3\2\62;\3\2")
+        buf.write("\63;\5\2\13\f\17\17\"\"\2>\2\3\3\2\2\2\2\13\3\2\2\2\2")
+        buf.write("\r\3\2\2\2\2\17\3\2\2\2\2\21\3\2\2\2\3#\3\2\2\2\5%\3\2")
+        buf.write("\2\2\7*\3\2\2\2\t-\3\2\2\2\13\60\3\2\2\2\r\66\3\2\2\2")
+        buf.write("\179\3\2\2\2\21;\3\2\2\2\23$\5\5\3\2\24\25\5\t\5\2\25")
+        buf.write("\36\5\7\4\2\26\30\7a\2\2\27\31\t\2\2\2\30\27\3\2\2\2\31")
+        buf.write("\32\3\2\2\2\32\30\3\2\2\2\32\33\3\2\2\2\33\35\3\2\2\2")
+        buf.write("\34\26\3\2\2\2\35 \3\2\2\2\36\34\3\2\2\2\36\37\3\2\2\2")
+        buf.write("\37!\3\2\2\2 \36\3\2\2\2!\"\b\2\2\2\"$\3\2\2\2#\23\3\2")
+        buf.write("\2\2#\24\3\2\2\2$\4\3\2\2\2%&\7\62\2\2&\6\3\2\2\2\')\t")
+        buf.write("\2\2\2(\'\3\2\2\2),\3\2\2\2*(\3\2\2\2*+\3\2\2\2+\b\3\2")
+        buf.write("\2\2,*\3\2\2\2-.\t\3\2\2.\n\3\2\2\2/\61\t\4\2\2\60/\3")
+        buf.write("\2\2\2\61\62\3\2\2\2\62\60\3\2\2\2\62\63\3\2\2\2\63\64")
+        buf.write("\3\2\2\2\64\65\b\6\3\2\65\f\3\2\2\2\66\67\13\2\2\2\67")
+        buf.write("8\b\7\4\28\16\3\2\2\29:\13\2\2\2:\20\3\2\2\2;<\13\2\2")
+        buf.write("\2<\22\3\2\2\2\b\2\32\36#*\62\5\3\2\2\b\2\2\3\7\3")
         return buf.getvalue()
 
 
@@ -46,7 +45,7 @@ class BKOOLLexer(Lexer):
 
     decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
 
-    BKNETID = 1
+    INTLIT = 1
     WS = 2
     ERROR_CHAR = 3
     UNCLOSE_STRING = 4
@@ -60,9 +59,9 @@ class BKOOLLexer(Lexer):
  ]
 
     symbolicNames = [ "<INVALID>",
-            "BKNETID", "WS", "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
+            "INTLIT", "WS", "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
 
-    ruleNames = [ "BKNETID", "NAM_LAN", "HO_TEN", "CHUOI", "CUOI", "WS", 
+    ruleNames = [ "INTLIT", "ONLY_ZERO", "NUMBER", "START_NO_ZERO", "WS", 
                   "ERROR_CHAR", "UNCLOSE_STRING", "ILLEGAL_ESCAPE" ]
 
     grammarFileName = "BKOOL.g4"
@@ -78,7 +77,8 @@ class BKOOLLexer(Lexer):
     def action(self, localctx:RuleContext, ruleIndex:int, actionIndex:int):
         if self._actions is None:
             actions = dict()
-            actions[6] = self.ERROR_CHAR_action 
+            actions[0] = self.INTLIT_action 
+            actions[5] = self.ERROR_CHAR_action 
             self._actions = actions
         action = self._actions.get(ruleIndex, None)
         if action is not None:
@@ -87,8 +87,13 @@ class BKOOLLexer(Lexer):
             raise Exception("No registered action for:" + str(ruleIndex))
 
 
-    def ERROR_CHAR_action(self, localctx:RuleContext , actionIndex:int):
+    def INTLIT_action(self, localctx:RuleContext , actionIndex:int):
         if actionIndex == 0:
+            self.text = self.text.replace("_","")
+     
+
+    def ERROR_CHAR_action(self, localctx:RuleContext , actionIndex:int):
+        if actionIndex == 1:
             raise ErrorToken(self.text)
      
 
